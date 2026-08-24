fe6f5a329908d0ee5dc5fbb86079152af7f75926
naeemahmaddev426 <naeemahmaddev426>
2026-08-24 20:47:35 +0500

    Final project Complete

---
+++ config/properties.php
@@ -0,0 +1,16 @@
+<?php
+
+return [
+    ['ref' => '22', 'address' => 'York Co 31 Burnage Lane, Burnage, Manchester, M19 2HZ', 'type' => 'Flat', 'status' => 'Let'],
+    ['ref' => '31', 'address' => '17 Abbey Drive, Moston, Manchester, M40 0FG', 'type' => 'End-Terraced', 'status' => 'Let'],
+    ['ref' => '32', 'address' => '44 Abbey Hey Lane, Abbey Hey, Manchester, M18 8TH', 'type' => 'Terraced', 'status' => 'Let'],
+    ['ref' => '25', 'address' => 'Adswood Road, Cheadle, Stockport, SK8 5QA', 'type' => 'Semi-Detached', 'status' => 'Available'],
+    ['ref' => '13', 'address' => 'Bakewell Street, Gorton, Manchester, M18 7AT', 'type' => 'Terraced', 'status' => 'Let'],
+    ['ref' => '40', 'address' => 'Bakewell Street, Gorton, Manchester, M18 7AQ', 'type' => 'End-Terraced', 'status' => 'Let'],
+    ['ref' => '34', 'address' => 'Bluestone Road, Moston, Manchester, M9 0JD', 'type' => 'Terraced', 'status' => 'Available'],
+    ['ref' => '15', 'address' => 'Cheddar Street, Gorton, Manchester, M18 8QB', 'type' => 'End-Terraced', 'status' => 'Available'],
+    ['ref' => '111', 'address' => 'Cleveland Road, Crumpsall, Manchester, M8 4GX', 'type' => 'Terraced', 'status' => 'Under Offer'],
+    ['ref' => 'Flat 2', 'address' => 'Roch Bank, Blackley, Manchester, M9 8FL', 'type' => 'Flat', 'status' => 'Let'],
+    ['ref' => '71', 'address' => 'Waterloo Road, Cheetham Hill, Manchester, M8 8BY', 'type' => 'Flat', 'status' => 'Let'],
+    ['ref' => '43', 'address' => 'Woodhouse Street, Gorton, Manchester, M18 8PA', 'type' => 'Terraced', 'status' => 'Under Offer'],
+];
